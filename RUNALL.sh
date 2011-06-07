fce0b17950fd22be5992cba4c3a6036859bda63e
David Anderson <davea42@earthlink.net>
2011-06-06 18:24:54 -0700

    Added start/end time to the end of the RUNALL.sh output.
    dwarfdump.c: Changed the missing-producer-attribute
            default string used in summary/error
            outputs to  "<CU-missing-DW_AT_producer>"
            so it is clearer.

--- RUNALL.sh
+++ RUNALL.sh
@@ -1,7 +1,11 @@
 rm -f ALLdd 
 rm -f ALLdd2 
 rm -f ALLddtodd2
+start=`date`
 ./DWARFTEST.sh dd 2>ALLdd 1>&2
 ./DWARFTEST.sh dd2 2>ALLdd2 1>&2
 #The following temporarily set aside.
 #./DWARFTEST.sh ddtodd2 2>ALLddtodd2 1>&2
+endt=`date`
+echo "start $start"
+echo "end   $endt"
