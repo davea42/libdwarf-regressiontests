ee2304ced9a5215f2f0128658590445ad48f26c5
David Anderson <davea42@earthlink.net>
2016-08-26 16:52:28 -0700

    Refining and correcting tests so we test relocations more thoroughly, with a test on .debug_strp creation added. offsetfromlowpc/basehighpc1 offsetfromlowpc/basestrpgenout offsetfromlowpc/basestrpiM offsetfromlowpc/runtest.sh

--- offsetfromlowpc/runtest.sh
+++ offsetfromlowpc/runtest.sh
@@ -11,6 +11,8 @@ sim=$3
 
 tx=junktesthipcoffset.o
 ty=junktesthipcoffsetorig.o
+tz=junktesthipcoffsetstrp.o
+ta=junkstrp.iM
 t1=junk1.highpc
 t2=junk2.highpc
 t3=junk3.basehighpc3
@@ -19,9 +21,13 @@ t5=junkh.basehighpc1
 t6=junk2h.basehighpc2
 t7=junk.basehighpc4
 t8=junk8.genout
+t8e=junk8.genoutstderr
 t9=junkstrp.genout
-ta=junkstrp.iM
+t9e=junkstrp.genoutstderr
 rm -f $tx
+rm -f $ty
+rm -f $tz
+rm -f $ta
 rm -f $t1
 rm -f $t2
 rm -f $t3
@@ -30,10 +36,9 @@ rm -f $t5
 rm -f $t6
 rm -f $t7
 rm -f $t8
+rm -f $t8e
 rm -f $t9
-rm -f $ta
-rm -f $tx
-rm -f $ty
+rm -f $t9e
 
 if [ ! -x $gen ]
 then
@@ -53,32 +58,45 @@ fi
 
 $gen  -h -t obj -c 0 -o $tx dwarfdump.o  >$t4 2>/dev/null
 
-# This one no conversion.
-$gen   -t obj -c 0 -o $ty dwarfdump.o  >$t8 2>/dev/null
-$dd -i -M dwarfdump.o >$t1 2> /dev/null
+# This one no conversion, DW_FORM_string.
+$gen -r  -t obj -c 0 -o $ty dwarfdump.o  >$t8 2> $t8e
+$dd -i -M $ty  >$t1 2> /dev/null
 
 # gens DW_FORM_strp
-$gen  -s  -t obj -c 0 -o $ty dwarfdump.o  >$t9 2>/dev/null
-$dd -i -M dwarfdump.o >$ta 2> /dev/null
+echo $gen -r  -s  -t obj -c 0 -o $tz dwarfdump.o  
+$gen -r  -s  -t obj -c 0 -o $tz dwarfdump.o  >$t9 2> $t9e
+echo Now show FORM_str and more for $tz >$ta
+$dd -i -M $tz >>$ta 2> /dev/null
 
 diff basestrpiM  $ta
 if [ $? -ne 0 ]
 then
-    echo FAIL diff basestrpiM $ta
+    echo FAIL diff basestrpiM $ta . Object gen is $tz
     echo FAIL offsetfromlowpc/runtest.sh Generating strp strings
     echo "to update baseline do: mv $ta basestrpiM"
     exit 1
 fi
 
+diff basestrpgenout  $t9
+if [ $? -ne 0 ]
+then
+    echo FAIL diff basestrpiM $ta. Object gen is $tz
+    echo FAIL offsetfromlowpc/runtest.sh Generating strp strings
+    echo "to update baseline do: mv $t9 basestrpgenout"
+    exit 1
+fi
+
 
 grep high_pc $t1 >$t5
+echo Now show FORM_str for $ty >> $t5
+grep DW_FORM_str $t1 >>$t5
 
 diff  basehighpc1 $t5
 if [ $? -ne 0 ]
 then
-    echo FAIL diff basehighpc1 $t5
+    echo FAIL diff basehighpc1 $t5 object gen is $ty
     echo FAIL offsetfromlowpc/runtest.sh  incorrect list of high_pc data.
-    echo "to update baseline do: mv $t5 basehipc1"
+    echo "to update baseline do: mv $t5 basehighpc1"
     exit 1
 fi
 
