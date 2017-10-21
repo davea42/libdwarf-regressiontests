c3c1db712dd6969c0ecf683e4a7546fbebb7da1f
David Anderson <davea42@linuxmail.org>
2017-10-21 13:18:07 -0700

    Now the new data16/ test should work ok in all test environments normally used. 	modified:   data16-2.base 	modified:   data16.base 	modified:   runtest.sh 	new file:   test.bin

--- data16/runtest.sh
+++ data16/runtest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # We name temp files junk<something> as that way
-# a normal make distclean gets rid of them
+# a normal 'make distclean' gets rid of them
 
 dd=../dwarfdump
 dg=../dwarfgen
+
+# First a basic test that we can add data16
+# to a trivial object file.
 cc -g -O0 -c test.c
 if [ $? -ne 0 ]
 then
@@ -12,16 +15,41 @@ then
   exit 1
 fi
 mv test.o junktest.o
+$dd -i -M junktest.o >junktest.ddo 
+grep DW_FORM_data16 <junktest.ddo 1>/dev/null 2>/dev/null
+if [ $? -eq 0 ]
+then
+ echo FAIL data16 already have DW_FORM_data16
+ exit 1
+fi
+
 $dg --adddata16 -t obj -c 0 -o junk.bin  ./junktest.o 1>junk1.out 2>junkstderr.out 
 if [ $? -ne 0 ]
 then
   echo FAIL data16 build junk.bin from dwarfgen
   exit 1
 fi
-$dd -i -M junk.bin  >junkdata16.new
+$dd -i -M junk.bin  >junkdata16.new 2>/dev/null
 if [ $? -ne 0 ]
 then
   echo FAIL data16 dwarfdump junk.bin from dwarfgen
+  exit 1
+fi
+grep DW_FORM_data16 <junkdata16.new 1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]
+then
+ echo FAIL data16 failed to add data16 to junktest.o  by dwarfgen
+ exit 1
+fi
+
+
+
+# Now ensure that the full dwarfdump output is unchanged
+# measured against a saved object: test.bin
+$dd -i -M  test.bin  >junkdata16.new 2>/dev/null
+if [ $? -ne 0 ]
+then
+  echo FAIL data16 dwarfdump -i -M data16.bin from dwarfgen
   exit 1
 fi
 
@@ -33,26 +61,35 @@ then
   exit 1
 fi
 
-# This checks if dwarfgen works on data16
-$dg -t obj -c 0 -o junk2.bin  ./junk.bin 1>junk2.out 2>junk2stderr.out
+
+# This checks if dwarfgen works on test.bin
+$dg -t obj --adddata16 -c 0 -o junk3.bin  ./test.bin 1>junk2.out 2>junk2stderr.out
 if [ $? -ne 0 ]
 then
-  echo FAIL data16 build junk2.bin  from dwarfgen
+  echo FAIL data16 build junk2.bin  from junk.bin via dwarfgen
   exit 1
 fi
 
-$dd -i -M junk2.bin  >junk2data16.new
+#Now lets see if adding data16 worked.
+$dd -i -M junk3.bin  >junk2data16.new 2>/dev/null
 if [ $? -ne 0 ]
 then
-  echo FAIL data16 dwarfdump junk2.bin from dwarfgen
+  echo FAIL data16 dwarfdump on junk3.bin from dwarfgen
   exit 1
 fi
 
-diff data16-2.base junk2data16.new  1>/dev/null 2>/dev/null
+grep DW_FORM_data16 <junk2data16.new 1>junk4data16.new 2>/dev/null
 if [ $? -ne 0 ]
 then
-  echo "FAIL data16 diff data16-2.base junk2data16.new"
-  echo "If data16.new is correct do: mv junk2data16.new data16-2.base"
+ echo FAIL data16 failed to add data16 to test.bin by dwarfgen
+ exit 1
+fi
+
+diff data16-2.base junk4data16.new  1>/dev/null 2>/dev/null
+if [ $? -ne 0 ]
+then
+  echo "FAIL data16 diff data16-2.base junk4data16.new"
+  echo "If data16.new is correct do: mv junk4data16.new data16-2.base"
   exit 1
 fi
 
