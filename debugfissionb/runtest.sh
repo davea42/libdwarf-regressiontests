8ec2aef0dd66490275b82c9da441fa264075e21c
David Anderson <davea42@linuxmail.org>
2019-01-03 15:33:29 -0800

    New tests of libdwarf dwarf_init_path() using simplereader Some not yet fully correct baselines. 	new file:   debugfissionb/moa.base 	new file:   debugfissionb/moax64.base 	new file:   debugfissionb/moddg4.base 	new file:   debugfissionb/moobject32.base 	new file:   debugfissionb/moobject64.base 	new file:   debugfissionb/pedll.base 	new file:   debugfissionb/peexe64.base 	modified:   debugfissionb/runtest.sh

--- debugfissionb/runtest.sh
+++ debugfissionb/runtest.sh
@@ -10,7 +10,7 @@ then
    echo "FAIL: executable not marked executable: $1"
    exit 1
 fi
-dd=$1
+sr=$1
 . ../BASEFILES
 isfail="n"
 # Avoid spurious differences because of the names of the
@@ -43,7 +43,7 @@ m() {
   opts=$3
   obj=./ld-new.dwp
   tmp=junk.${baseline}
-  $dwdumper $opts $obj 1> ${tmp} 2>&1
+  $sr $opts $obj 1> ${tmp} 2>&1
   r=$?
   if test  $r  -ne 0
   then
@@ -59,15 +59,48 @@ m() {
   fi
 }
 
-m $dd thash1.base '--tuhash=058027967850060b'
-m $dd thash2.base '--tufissionhash=b1fcaeaf1d01cc85'
-m $dd chash1.base '--cuhash=032ccfcfbc7c9d26'
-m $dd chash2.base '--cufissionhash=0441e597e1e38549'
+m $sr thash1.base '--tuhash=058027967850060b'
+m $sr thash2.base '--tufissionhash=b1fcaeaf1d01cc85'
+m $sr chash1.base '--cuhash=032ccfcfbc7c9d26'
+m $sr chash2.base '--cufissionhash=0441e597e1e38549'
 # We have the hash arg so output is limited in size.
 # The following two are  not much of a test of options. 
 # Just a simple basic run.
-m $dd chash2.base '--cufissionhash=0441e597e1e38549' '--passnullerror'
-m $dd chash2.base '--cufissionhash=0441e597e1e38549' '--passnullerror' '--simpleerrhand'
+m $sr chash2.base '--cufissionhash=0441e597e1e38549' '--passnullerror'
+m $sr chash2.base '--cufissionhash=0441e597e1e38549' '--passnullerror' '--simpleerrhand'
+
+m2() {
+  mysr=$1
+  baseline=$2
+  obj=$3
+  if [ ! -f $baseline ]
+  then
+    touch $baseline
+  fi
+  tmp=junk.${baseline}
+  $mysr $obj 1> ${tmp} 2>&1
+  r=$?
+  if test  $r  -ne 0
+  then
+      echo exit_status  $r >>$tmp
+  fi
+  diff $baseline $tmp
+  if test  $?  -ne 0
+  then
+      echo "FAIL test $baseline vs $tmp"
+      echo "to update, mv $tmp $baseline"
+      isfail="y"
+      #exit 1
+  fi
+}
+
+m2 $sr moa.base ../macho-kask/simplereaderi386 
+m2 $sr moax64.base ../macho-kask/simplereaderx86_64 
+m2 $sr moobject32.base ../macho-kask/mach-o-object32 
+m2 $sr moobject64.base ../macho-kask/mach-o-object64
+m2 $sr moddg4.base ../macho-kask/dwarfdump_g4 
+m2 $sr pedll.base ../pe1/libexamine-0.dll 
+m2 $sr peexe64.base ../pe1/kask-dwarfdump_64.exe
 
 if [ $isfail = "y" ]
 then
