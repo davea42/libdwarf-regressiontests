a4de02279c1d5c0d5b314a950b23ca4a2c022bc4
David Anderson <davea42@linuxmail.org>
2019-01-26 14:09:58 -0800

    A new test that dwarfdump can read section headers from an object with size  above 2GB. By building the big one from a fixed known small one.
    Where the section headers themselves are above the 2GB
    offset and the original section headers (as copied
    from the small object hello) are zeroed out..
    We create the big one from a small one and delete
    the big one once the test runs.
    	modified:   CLEANUP
    	modified:   bigobj/CLEANUP
    	new file:   bigobj/bigobj.base
    	new file:   bigobj/runtest.sh

---
+++ bigobj/runtest.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Creates a temp object bigobject which is
+# over 2GB in size from the executable "hello".  
+# Moves the section headers
+# from hello up high in bigobject, over the 2GB size
+# and zeros out the original section headers to guarantee
+# that we will read the new copy or fail.
+# Then verifies dwarfdump works.
+
+dd=../dwarfdump
+dd=dwarfdump
+bn=junkbigobj.new
+bb=bigobj.base
+bo=bigobject
+rm -f makebig
+rm -f bigobject
+rm -f $bn
+
+gcc makebig.c -o makebig
+if [ $? -ne 0 ]
+then
+  echo "FAIL bigobject compile of makebig.c"
+  exit 1
+fi
+
+./makebig 2500000000
+if [ $? -ne 0 ]
+then
+  echo "FAIL bigobject creation"
+  rm -f makebig
+  exit 1
+fi
+
+if [ ! -f ./bigobject ]
+then
+  echo "FAIL the file bigobject does not exist"
+  rm -f makebig
+  exit 1
+fi
+
+$dd bigobject  1> $bn 2>&1 
+if [ $? -ne 0 ]
+then
+  echo "FAIL  dwarfdump of bigobject"
+  rm -f bigobject
+  rm -f makebig
+  exit 1
+fi
+
+#rm -f bigobject
+
+diff $bb $bn
+if [ $? -ne 0 ]
+then
+  echo "FAIL bigobject diff $bb $bn"
+  echo "to update: mv $bn $bb"
+  rm -f bigobject
+  rm -f makebig
+  exit 1
+fi
+rm -f $bn
+rm -f bigobject
+rm -f makebig
+rm -f a.out
+echo PASS
+exit 0
+
