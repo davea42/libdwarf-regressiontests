eb7dcdd52077788dbf8ee0bc99fe96b0a1983a4e
David Anderson <davea42@linuxmail.org>
2019-09-09 13:12:59 -0700

    Now we also so the GNU build-id when the section exists.
    	new file:   basecrcoutb
    	modified:   baselink
    	new file:   baselinkc
    	modified:   runtest.sh

--- debuglink/runtest.sh
+++ debuglink/runtest.sh
@@ -18,6 +18,15 @@ then
     exit 1
 fi
 
+$dd --print-gnu-debuglink crc32.debug >junklinkc
+diff baselinkc junklinkc
+if [ $? -ne 0 ]
+then
+    echo "FAIL debuglink base link wrong"
+    echo "To update: mv junklinkc baselinkc"
+    exit 1
+fi
+
 cc $nli crc32.c -o junkcrc32 > junkcompilestdout 2> junkcompileoutstderr
 if [ $? -ne 0 ]
 then
@@ -28,8 +37,16 @@ fi
 diff basecrcout junkcrcout 
 if [ $? -ne 0 ]
 then
-    echo "FAIL debuglink crc out wrong"
+    echo "FAIL debuglink etc on crc32.debug wrong"
     echo "To update: mv junkcrcout basecrcout"
+    exit 1
+fi
+./junkcrc32 crc32 > junkcrcoutb
+diff basecrcoutb junkcrcoutb
+if [ $? -ne 0 ]
+then
+    echo "FAIL debuglink etc crc32 out wrong"
+    echo "To update: mv junkcrcoutb basecrcoutb"
     exit 1
 fi
 echo "PASS debuglink/runtest.sh" 
