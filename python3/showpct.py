088786c4e2623a41841da3fdccb7730e21aba62c
David Anderson <davea42@linuxmail.org>
2020-07-02 16:33:42 -0700

    latest result 	modified:   RegressionTestsResults.ods
    latest baselines
    	modified:   dwarfdump-i386-freebsd.O
    	modified:   dwarfdump-i386-ubuntu.O
    	modified:   dwarfdump-x86_64-freebsd.O
    	modified:   dwarfdump-x86_64-ubuntu.O
    
    Test count higher so increased the denominator
    for percent calculation
    	modified:   python2/showpct.py
    	modified:   python3/showpct.py

--- python3/showpct.py
+++ python3/showpct.py
@@ -3,7 +3,7 @@ import sys
 
 # An approximation of the actual full count.
 # We just see half the total for the pct figurel
-realtotal= 51000.0 
+realtotal= 53004.0
 # adjust the true total so we don't see 100 pct
 # too early...or maybe at all. 99 is good :-)
 # The count we see input is *half* the true total.
