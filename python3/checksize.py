f3c223934ba78863f30ab8dd0f1be24ce1f136d2
David Anderson <davea42@linuxmail.org>
2020-11-08 10:01:39 -0800

    Both had bugs. Oops. 	modified:   python2/checksize.py 	modified:   python3/checksize.py

--- python3/checksize.py
+++ python3/checksize.py
@@ -8,24 +8,27 @@ def getSize(name):
   try:
     file = open(name,"r")
   except IOError as message:
-    print("FAIL open ",fn,"errormessage",message)
-    sys.exit(1)
+    print("FAIL open ",name,"errormessage",message)
+    return 0
   file.seek(0,2) # move the cursor to the end of the file
   size = file.tell()
   file.close()
   return size
 
 if __name__ == '__main__':
-  fsz = sys.argv[1]
-  if not fsz.isdigit():
-    print("checksize.py first arg not a number")
+  if len(sys.argv) < 3:
+    print("Too few args to checksize.py:",len(sys.argv))
+    sys.exit(0)
+  maxsz = sys.argv[1]
+  if not maxsz.isdigit():
+    print("checksize.py first arg not a number, is",maxsz)
     sys.exit(0)
   fname = sys.argv[2]
   sz1 = getSize(fname)
   fname2 = sys.argv[3]
   sz2 = getSize(fname2)
-  if int(sz1) > int(fsz):
+  if int(sz1) > int(maxsz):
      sys.exit(1)
-  if int(sz2) > int(fsz):
+  if int(sz2) > int(maxsz):
      sys.exit(1)
   sys.exit(0)
