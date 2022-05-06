1df82d66f759a31067db31144599d1fb5ffc5af9
David Anderson <davea42@linuxmail.org>
2022-05-06 13:35:53 -0700

    in guilfanov2/ new file:   README new file:   details.txt new file:   double-free-poc new file:   poc.base new file:   runtest.sh

---
+++ guilfanov2/runtest.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ../BASEFILES.sh
+ts=$testsrc/guilfanov
+tf=$bldtest/guilfanov
+
+../dwarfdump -a $ts/libdwarf_crash.elf >junkn
+diff $ts/crash.base junkn
+if [ $? -ne 0 ]
+then
+  echo "FAIL dwarfdump -a -v -M guilfanov/libdwarf_crash.elf"
+  echo "To update baseline: mv $tf/junkn $ts/crash.base"
+  echo "rerun: $ts/runtest.sh"
+  return 1
+fi
+return 0
