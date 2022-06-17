ad28efa8e3e1f1608df55e2465b57078a6c490ce
David Anderson <davea42@linuxmail.org>
2022-06-17 12:38:49 -0700

    Had to remove full paths from echo stmts to remove file paths from the baseline so it can pass on S390 linux tests. testfindfuncbypc: 	modified:   findfuncbypc.base 	modified:   runall.sh

--- testfindfuncbypc/runall.sh
+++ testfindfuncbypc/runall.sh
@@ -10,15 +10,15 @@ echo "====Missing path"
 echo "====$ff --printdetails --pc=1"
 $ff --printdetails --pc=1
 echo "====pc not found, no output"
-echo "====$ff --printdetails --pc=1 $testff"
+echo "====$ff --printdetails --pc=1 findfuncbypc.ex1"
 $ff --printdetails --pc=1 $testff
 echo "====pc not found, no output"
-echo "====$ff --printdetails $testff"
+echo "====$ff --printdetails findfuncbypc.ex1"
 $ff --printdetails $testff
 echo "====pc not found, no output"
-echo "====$ff --printdetails --pc=10000 $testff"
+echo "====$ff --printdetails --pc=10000 findfuncbypc.ex1"
 $ff --printdetails --pc=10000 $testff
 echo "====Following tests using ranges properly"
-echo "====$ff --printdetails --pc=0x36a4 $testff"
+echo "====$ff --printdetails --pc=0x36a4 findfuncbypc.ex1"
 $ff --printdetails --pc=0x36a4 $testff
 
