6b5a09600f278bcc5e53837b6709c0bf6a18976b
David Anderson <davea42@linuxmail.org>
2022-10-11 10:24:04 -0700

    New test case 	new file:   README 	new file:   main 	new file:   main.cc 	new file:   test.h

---
+++ issue137gh/main.cc
@@ -0,0 +1,10 @@
+
+#include <cstdio>
+#include "test.h"
+
+int main(int argc, char* * argv) {
+    int a = inline_add(1);
+    printf("%d\n", a);
+}
+
+
---
+++ issue137gh/test.h
@@ -0,0 +1,18 @@
+
+#ifndef TEST__H_
+#define TEST__H_
+
+// Deliberately left blank lines, so that the number of lines where the inline
+// function is located is greater than all the lines of code in main.cc
+
+
+
+
+
+
+static inline int inline_add(int a) {
+    return ++a;
+}
+
+
+#endif
