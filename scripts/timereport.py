27034d7e87fd009103b038ddc4059a7805005cb1
David Anderson <davea42@linuxmail.org>
2022-12-27 14:51:21 -0800

    Now somewhat useful. 	modified:   timereport.py

--- scripts/timereport.py
+++ scripts/timereport.py
@@ -2,28 +2,29 @@
 #
 #  Report on the timings in an ALLdd file
 #  to try to figure out what is slow
-# 
+#
 
 import sys
 import os
-#import datetime
+
+# import datetime
 from datetime import timedelta
 from datetime import datetime
 
 
-#=====START Pct   0  -a  kapus/bad.obj
-#old start  Fri Jun 26 16:51:29 PDT 2020
-#old done  Fri Jun 26 16:51:29 PDT 2020
-#new start  Fri Jun 26 16:51:29 PDT 2020
-#new done  Fri Jun 26 16:51:29 PDT 2020
-#Fri Jun 26 16:51:29 PDT 2020
-#=====START Pct   0 --print_raw_rnglists  moya2/filecheck.dwo
-#old start  Fri Jun 26 16:51:29 PDT 2020
-#old done  Fri Jun 26 16:51:29 PDT 2020
-#new start  Fri Jun 26 16:51:29 PDT 2020
-#new done  Fri Jun 26 16:51:29 PDT 2020
-#Fri Jun 26 16:51:29 PDT 2020
-#=====START Pct   0  -ka --print_raw_rnglists  moya2/filecheck.dwo
+# =====START Pct   0  -a  kapus/bad.obj
+# old start  Fri Jun 26 16:51:29 PDT 2020
+# old done  Fri Jun 26 16:51:29 PDT 2020
+# new start  Fri Jun 26 16:51:29 PDT 2020
+# new done  Fri Jun 26 16:51:29 PDT 2020
+# Fri Jun 26 16:51:29 PDT 2020
+# =====START Pct   0 --print_raw_rnglists  moya2/filecheck.dwo
+# old start  Fri Jun 26 16:51:29 PDT 2020
+# old done  Fri Jun 26 16:51:29 PDT 2020
+# new start  Fri Jun 26 16:51:29 PDT 2020
+# new done  Fri Jun 26 16:51:29 PDT 2020
+# Fri Jun 26 16:51:29 PDT 2020
+# =====START Pct   0  -ka --print_raw_rnglists  moya2/filecheck.dwo
 # Compute the seconds from old start to new done
 # Compute the seconds from new done to old start.
 # Compute the seconds from the very first old start
@@ -31,142 +32,196 @@ from datetime import datetime
 
 
 class xtime:
-  def __init__(self):
-    # These are all datetime objects
-    self._firstostart = False
-    self._lastnewdone = False
-    self._curostart = False
-    self._curndone = False
-    self._curfile = "<none>"
-    self._testcount = 0
+    def __init__(self):
+        # These are all datetime objects
+        self.reset()
 
-    # code or shell runtime > this  will be called 'notable'
-    self._notablekey = 20
+    def reset(self):
+        self._command = False
+        self._newstart = False
+        self._newdone = False
+        self._newduration = False
+        self._oldstart = False
+        self._olddone = False
+        self._testnum = False
+        self._oldduration = False
 
-mdict = { "Jan":1, "Feb":2, "Mar":3, "Apr":4, 
-"May":5, "Jun":6, "Jul":7, "Aug":8,
-"Sep":9, "Oct":10, "Nov":11, "Dec":12 }
+    def repr(self, which):
+        txt = "{dur:03d} {num:n} {com}"
+        if which == "old":
+            return txt.format(
+                dur=self._oldduration, num=int(self._testnum), com=self._command
+            )
+        else:
+            return txt.format(
+                dur=self._newduration, num=int(self._testnum), com=self._command
+            )
+
+        # code or shell runtime > this  will be called 'notable'
+        self._notablekey = 20
+
+
+mdict = {
+    "Jan": 1,
+    "Feb": 2,
+    "Mar": 3,
+    "Apr": 4,
+    "May": 5,
+    "Jun": 6,
+    "Jul": 7,
+    "Aug": 8,
+    "Sep": 9,
+    "Oct": 10,
+    "Nov": 11,
+    "Dec": 12,
+}
+
 
 def extractdate(rec):
-  wds = rec.split()
-  mt = wds[3]
-  month = mdict[mt]
-  day = wds[4]
-  year = wds[7]
-  tm = wds[5]
-  tmwds = tm.split(":")
-  dt = datetime(int(year),int(month),int(day),\
-    int(tmwds[0]),int(tmwds[1]),int(tmwds[2]))
-  return dt
-
-def recordcurrent(rec,xt):
-  if rec.startswith("=====START Pct "):
     wds = rec.split()
-    n = ' '.join(wds[1:])
-    xt._curfile = n
-    return True
-  if rec.startswith("old start "):
-    #print("dadebug",rec);
-    dt = extractdate(rec)
-    if not xt._firstostart:
-      xt._firstostart = dt 
-    xt._curostart = dt
-    xt._testcount = int(xt._testcount) +1
-    if xt._curndone:
-      td = dt - xt._curndone
-      print("Shell tm",td,xt._curfile)
-      if td.total_seconds() > xt._notablekey:
-        print("=====shellt notable",td,xt._curfile)
-    return True
-  if rec.startswith("old done "):
-    dt = extractdate(rec)
-    return False
-  if rec.startswith("new start "):
-    dt = extractdate(rec)
-    return False
-  if rec.startswith("new done "):
-    #print("dadebug",rec);
-    dt = extractdate(rec)
-    xt._lastnewdone = dt 
-    xt._curndone = dt
-    if xt._curostart:
-      td = dt - xt._curostart
-      print("Run time",td,xt._curfile)
-      if td.total_seconds() > float(xt._notablekey):
-        print("=====runtime notable",td,xt._curfile)
-    return True
-  return False
+    dt = wds[2]
+    dtwds = dt.split("-")
+    #print("dtwds", dtwds)
+    y = dtwds[0]
+    m = dtwds[1]
+    d = dtwds[2]
+
+    tm = wds[3]
+    tmwds = tm.split(":")
+    #print("tmwds", tmwds)
+    h = tmwds[0]
+    min = tmwds[1]
+    s = tmwds[2]
+
+    dt = datetime(int(y), int(m), int(d), int(h), int(min), int(s))
+    return dt
+
+
+def recordcurrent(rec, xt):
+    if rec.startswith("=====START Pct "):
+        xt.reset()
+        wds = rec.split()
+        n = " ".join(wds[3:])
+        xt._command = n
+        return False, False
+    if rec.startswith("=====STATS Pct "):
+        wds = rec.split()
+        n = wds[4]
+        xt._testnum = n
+        return False, False
+    if rec.startswith("old start "):
+        # print("dadebug",rec);
+        dt = extractdate(rec)
+        xt._oldstart = dt
+        return False, False
+
+    if rec.startswith("old done "):
+        dt = extractdate(rec)
+        xt._olddone = dt
+        df = xt._olddone - xt._oldstart
+        xt._oldduration = int(df.total_seconds())
+        return True, "old"
+
+    if rec.startswith("new start "):
+        dt = extractdate(rec)
+        xt._newstart = dt
+        return False, "new"
+
+    if rec.startswith("new done "):
+        dt = extractdate(rec)
+        xt._newdone = dt
+        df = xt._newdone - xt._newstart
+        xt._newduration = int(df.total_seconds())
+        return True, "new"
+    return False, False
 
 
 def isdateitem(rec):
-  if rec.startswith("old start "):
-    return True
-  if rec.startswith("old done "):
-    return True
-  if rec.startswith("new start "):
-    return True
-  if rec.startswith("new done "):
-    return True
-  if rec.startswith("Mon "):
-    return True
-  if rec.startswith("Tue "):
-    return True
-  if rec.startswith("Wed "):
-    return True
-  if rec.startswith("Thu "):
-    return True
-  if rec.startswith("Fri "):
-    return True
-  if rec.startswith("Sat "):
-    return True
-  if rec.startswith("Sun "):
-    return True
-  return False
+    if rec.startswith("old start "):
+        return True
+    if rec.startswith("old done "):
+        return True
+    if rec.startswith("new start "):
+        return True
+    if rec.startswith("new done "):
+        return True
+    if rec.startswith("Mon "):
+        return True
+    if rec.startswith("Tue "):
+        return True
+    if rec.startswith("Wed "):
+        return True
+    if rec.startswith("Thu "):
+        return True
+    if rec.startswith("Fri "):
+        return True
+    if rec.startswith("Sat "):
+        return True
+    if rec.startswith("Sun "):
+        return True
+    return False
+
 
 def printoverall(xt):
-  if not xt._firstostart:
-    print("Incomplete file")
     return
-  if not xt._lastnewdone:
-    print("Incomplete file")
-    return
-  td =  xt._lastnewdone -  xt._firstostart
-  print("Overall run time",td)
-  print("Test count      ",xt._testcount)
-  print("Seconds per test",td.total_seconds()/float(xt._testcount))
 
-if __name__ == '__main__':
-  fn="ALLdd"
-  if len(sys.argv) >1:
-    fn = sys.argv[1]
-  count=0
-  failcount = 0
-  print("Running timereport on file",fn)
-  try:
-    file = open(fn,"r")
-  except IOError as message:
-    print("FAIL open ",fn,"errormessage",message)
-    sys.exit(1)
-  maxreccount = 1000000
-  recordcount = 0
-  xt = xtime()
-  while 1:
+
+if __name__ == "__main__":
+    newduration = []
+    oldduration = []
+    fn = "ALLdd"
+    if len(sys.argv) > 1:
+        fn = sys.argv[1]
+    count = 0
+    failcount = 0
+    print("Running timereport on file", fn)
     try:
-      rec = file.readline().strip()
-    except EOFError:
-        break
-    if len(rec) < 1:
-      # eof
-      break
-    if int(recordcount) > maxreccount:
-      break
-    #if not isdateitem(rec):
-    #  continue 
-    x = recordcurrent(rec,xt)
-    if x:
-      recordcount = int(recordcount) + 1
-  printoverall(xt)  
+        file = open(fn, "r")
+    except IOError as message:
+        print("FAIL open ", fn, "errormessage", message)
+        sys.exit(1)
+    maxreccount = 1000000
+    recordcount = 0
+    xt = xtime()
+    while 1:
+        try:
+            rec = file.readline().strip()
+        except EOFError:
+            break
+        if len(rec) < 1:
+            # eof
+            break
+        if int(recordcount) > maxreccount:
+            break
+        # if not isdateitem(rec):
+        #  continue
+        x, onx = recordcurrent(rec, xt)
+        if x:
+            # end time.
 
+            #print(xt.repr(onx))
+            if onx == "old":
+                oldduration += [xt.repr(onx)]
+            elif onx == "new":
+                newduration += [xt.repr(onx)]
+            else:
+                pass
 
-
-
+        else:
+            # start time time.
+            if onx == "old":
+                pass
+            elif onx == "new":
+                pass
+            else:
+                pass
+    print("old len", len(oldduration))
+    print("new len", len(newduration))
+    newduration.sort()
+    newduration.reverse()
+    print("New list")
+    for n in newduration:
+      print(n)
+    print("Old list")
+    for n in oldduration:
+      print(n)
