73f53f23d35cff955c80692689c112ba1d197fc2
David Anderson <davea42@linuxmail.org>
2023-01-13 10:38:20 -0800

    This just used for occasional analysis, not normally run. Modified to be more useful right now. 	modified:   python3/showruntime.py

--- python3/showruntime.py
+++ python3/showruntime.py
@@ -14,10 +14,13 @@ import datetime
 def isdateitem(rec):
     if rec.startswith("=====START"):
         return "st"
-    if rec.startswith("old start "):
-        return "os"
+    #if rec.startswith("old start "):
+    #    return "os"
+    if rec.startswith("new start "):
+        return "ns"
     if rec.startswith("new done "):
         return "nd"
+    return False
 
 
 def extracttime(s):
@@ -27,25 +30,30 @@ def extracttime(s):
         sys.exit(1)
     fy = wds[2].split("-")
     ft = wds[3].split(":")
-    dt = datetime.datetime(
-        int(fy[0]), int(fy[1]), int(fy[2]), int(ft[0]), int(ft[1]), int(ft[2])
-    )
-    return dt
+    #dt = datetime.datetime(
+    #    int(fy[0]), int(fy[1]), int(fy[2]), int(ft[0]), int(ft[1]), int(ft[2])
+    secs = int(ft[0])*60*60 + \
+       int(ft[1]) *60 + \
+       int(ft[2]) 
+    #print("dadebug time secs\n",secs)
+    return secs,wds[3]
 
 
 if __name__ == "__main__":
-    fn = "ALLdd"
+    fn = "/var/tmp/dwtest/ALLdd"
     if len(sys.argv) > 1:
         fn = sys.argv[1]
     count = 0
     smallestinteresting = 5
+    totalseconds=0
     starttext = ""
     osd = False
     ndd = False
-    biggestrun = False
+    nds = False
+    biggestrun = 0
     biggestitem = ""
     failcount = 0
-    print("trimdate on file", fn)
+    print("file", fn)
     try:
         file = open(fn, "r")
     except IOError as message:
@@ -53,39 +61,57 @@ if __name__ == "__main__":
         sys.exit(1)
     while 1:
         try:
-            rec = file.readline().strip()
+            irec = file.readline()
         except EOFError:
             break
-        if len(rec) < 1:
+        if len(irec) < 1:
             # eof
             break
+        rec = irec.strip()
+        #print("dadebug rec read",rec);
         typerec = isdateitem(rec)
         if typerec == "st":
-            if osd and ndd:
-                diff = ndd - osd
-                if diff.total_seconds() > float(smallestinteresting):
-                    print(rec)
-                    print("dwarfdump secs", diff.total_seconds())
-                    if not biggestrun or diff > biggestrun:
-                        biggestrun = diff
-                        biggestitem = starttext
+            #if osd and ndd:
+            #    diff = ndd - osd
+            #    if diff.total_seconds() > float(smallestinteresting):
+            #        print(rec)
+            #        print("dwarfdump secs", diff.total_seconds())
+            #        if not biggestrun or diff > biggestrun:
+            #            biggestrun = diff
+            #            biggestitem = starttext
             starttext = rec
             # print(rec)
         elif typerec == "os":
-            osd = extracttime(rec)
-            if ndd:
-                diff = osd - ndd
-                if diff.total_seconds() > float(smallestinteresting):
-                    print(starttext)
-                    print("shell secs   ", diff.total_seconds())
-                    if not biggestrun or diff > biggestrun:
-                        biggestrun = diff
+            pass
+            #osd,t = extracttime(rec)
+            #if ndd:
+            #    diff = osd - ndd
+            #    if diff.total_seconds() > float(smallestinteresting):
+            #        print(starttext)
+            #        print("shell secs   ", diff.total_seconds())
+            #        if not biggestrun or diff > biggestrun:
+            #            biggestrun = diff
+        elif typerec == "ns":
+            # For the first new start, do this
+            if not ndd:
+              ndd,t = extracttime(rec)
         elif typerec == "nd":
-            ndd = extracttime(rec)
+            nddn,t = extracttime(rec)
+            #print("dadebug ndd osd",ndd,osd)
+            if ndd:
+              diff = nddn - ndd
+              print(starttext)
+              print("run secs   ", diff, t);
+              if diff > biggestrun:
+                  biggestrun = diff
+              totalseconds += diff
+            ndd = nddn
         else:
-            continue
-    if osd and ndd:
-        diff = ndd - osd
-        print("dwarfdump run time secs", diff.total_seconds())
-    print("biggest delta, seconds", biggestrun.total_seconds())
-    print("biggest was           ", biggestitem)
+            pass
+    #if osd and ndd:
+    #    diff = ndd - osd
+    #    print("dwarfdump run time secs", diff.total_seconds())
+    #print("biggest was           ", biggestitem)
+    print("biggest delta, seconds ", biggestrun)
+    print("total  seconds         ", totalseconds)
+    print("total  minutes         ", totalseconds/60)
