58424b5256b9852d6620af199885bd23a3ad7637
David Anderson <davea42@linuxmail.org>
2023-06-10 08:54:54 -0700

    New test for oss fuzz 59519 	modified:   DWARFTEST.sh 	new file:   baselines/ossfuzz59519.base 	new file:   ossfuzz59519/fuzz_set_frame_all-4670829255065600

--- DWARFTEST.sh
+++ DWARFTEST.sh
@@ -1078,6 +1078,8 @@ echo "=====BUILD  $testsrc/filelist/localfuzz_init_binary"
   chkres $? "check error compiled $testsrc/filelist/localfuzz_init_binary.c failed"
   cd ..
 
+runsingle ossfuzz59519.base  ./fuzz_set_frame_all --testobj=$testsrc/ossfuzz59519/fuzz_set_frame_all-4670829255065600
+
 runsingle ossfuzz59517.base  ./fuzz_set_frame_all --testobj=$testsrc/ossfuzz59517/fuzz_set_frame_all-5741671019839488
 
 runsingle ossfuzz59478.base  ./fuzz_set_frame_all --testobj=$testsrc/ossfuzz59478/fuzz_set_frame_all-5300774457180160
