b38f073881fd14b78fe31d85925de5b39a38998c
David Anderson <davea42@linuxmail.org>
2023-08-08 10:40:23 -0700

    Refined the test code to print a more detailed comment when executed. 	modified:   CmakeTest/test.c

--- CmakeTest/test.c
+++ CmakeTest/test.c
@@ -5,12 +5,26 @@ int main()
 {
     Dwarf_Debug dbg = 0;
     int result = 0;
+    const char *pathname = "folly";
 
     printf("test\n");
-    result = dwarf_init_path("folly",0,0,0,0,0,&dbg,0);
-    printf(" dwarf_init_path returned %d\n",result);
+    /*  The following call has to fail  as the pathname
+        is to a nonexistent file. */
+    result = dwarf_init_path(pathname,0,0,0,0,0,&dbg,0);
     if (result == DW_DLV_OK) {
+        printf("\ndwarf_init_path succeeded. Impossible"
+            " as we expect \'%s\' to be absent or invalid.\n",
+            pathname);
         dwarf_finish(dbg);
-    } 
+    } else if (result == DW_DLV_NO_ENTRY) {
+        printf("\ndwarf_init_path returned"
+            " DW_DLV_NO_ENTRY which is appropriate "
+            "as the file named \'%s\' does not exist\n",
+            pathname);
+    }  else {
+        printf("\ndwarf_init_path returned  DW_DLV_ERROR"
+            " which means the file \'%s\' existsd but is not the right"
+            " file type.\n",pathname);
+    }
     printf("Done\n");
 }
