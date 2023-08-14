9caea9e6de3c5762c7d881dd5266a956336fc57c
David Anderson <davea42@linuxmail.org>
2023-08-14 07:58:52 -0700

    Has example of a specific DWARF5 line table header feature: a header file index referring directly to a line table header directory index 0. 	new file:   debugso20230811.debug
    For utf8 testing.
    	new file:   utf8/README
    	new file:   utf8/test
    	new file:   utf8/test.c

---
+++ utf8/test.c
@@ -0,0 +1,14 @@
+/*  Copyright 2023  David Anderson All Rights Reserved.
+    This simple testcase is hereby placed in the
+    public domain for use by anyone for any purpose.
+*/
+
+
+
+int main()
+{
+    int Peña = 8;
+    int Guzmán =3;
+    int result = Peña-Guzmán;
+    return 0;
+}
