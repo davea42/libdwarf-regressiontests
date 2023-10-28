895b8301230c2e7b05b6655ab773782223b234ee
David Anderson <davea42@linuxmail.org>
2023-10-28 15:48:35 -0700

    These now do a small compile to determine where libz and libzstd, and returns an exit code meaning 'both in standard places' 'one or both missing' 'one or both in /opt/local in /usr/local' and adds data to BASENAMES.sh to be used when compiling during the testing. 	modified:   checkforlibz/runtest.sh 	modified:   checkforlibzstd/runtest.sh

--- checkforlibz/runtest.sh
+++ checkforlibz/runtest.sh
@@ -2,11 +2,50 @@
 . ../BASEFILES.sh
 # This does a trivial compile/link to see if zlib.h and -lz
 # exist for some test directories.
-cc $testsrc/checkforlibz/tzl.c -lz >junkstdout 2>junkstderr
+cc  $testsrc/checkforlibz/tzl.c   -lz >junkstdout 2>junkstderr
 if [ $? -eq 0 ]
 then
-  echo "FOUND: Found libz"
+  echo "FOUND: Found libz in standard locations"
   exit 0
 fi
+
+trycompile=n
+H=
+L=
+if test -d /opt/local/include ; then
+  H="-I/opt/local/include"
+  if test -d /opt/local/include ; then
+    L="-L/opt/local/lib"
+    trycompile="y"
+  fi
+fi
+
+if test $trycompile="y" ; then
+  cc $H $testsrc/checkforlibz/tzl.c $L -lz >junkstdout 2>junkstderr
+  if [ $? -eq 0 ]
+  then
+    echo "FOUND: Found libz in /opt/local"
+    exit 2
+  fi
+fi
+trycompile=n
+H=
+L=
+if test -d /usr/local/include ; then
+  H="-I/usr/local/include"
+  if test -d /usr/local/include ; then
+    L="-L/usr/local/lib"
+    trycompile=y
+  fi
+fi
+
+if test $trycompile=y ; then
+  cc $H $testsrc/checkforlibz/tzl.c $L -lz >junkstdout 2>junkstderr
+  if [ $? -eq 0 ]
+  then
+    echo "FOUND: Found libz in /opt/local"
+    exit 1
+  fi
+fi
 echo "NOT FOUND: libz"
-exit 1
+exit 3
--- checkforlibzstd/runtest.sh
+++ checkforlibzstd/runtest.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 . ../BASEFILES.sh
-# This does a trivial compile/link to see if zlib.h and -lz
+# This does a trivial compile/link to see if libzstd.h and -lzstd
 # exist for some test directories.
-cc $testsrc/checkforlibzstd/tzlstd.c -L/usr/lib -lzstd
+
+cc  $testsrc/checkforlibzstd/tzlstd.c   -lzstd >junkstdout 2>junkstderr
 if [ $? -eq 0 ]
 then
-  echo "FOUND: Found libzstd /usr/lib"
+  echo "FOUND: Found libzstd in standard locations"
   exit 0
 fi
-echo "Looking in /usr/local/lib: libzstd"
-cc $testsrc/checkforlibzstd/tzlstd.c -I/usr/local/include -L/usr/local/lib -lzstd
-if [ $? -eq 0 ]
-then
-  echo "FOUND: Found libzstd in /usr/local/lib libzstd"
-  # Horrrible use of exit code. Means FOUND here.
-  exit 2
+
+trycompile=n
+H=
+L=
+if test -d /opt/local/include ; then
+  H="-I/opt/local/include"
+  if test -d /opt/local/include ; then
+    L="-L/opt/local/lib"
+    trycompile="y"
+  fi
+fi
+
+if test $trycompile="y" ; then
+  echo "TRY compile "
+  cc $H $testsrc/checkforlibzstd/tzlstd.c $L -lzstd >junkstdout 2>junkstderr
+  if [ $? -eq 0 ]
+  then
+    echo "FOUND: Found libzstd in /opt/local"
+    exit 2
+  fi
+fi
+trycompile=n
+H=
+L=
+if test -d /usr/local/include ; then
+  H="-I/usr/local/include"
+  if test -d /usr/local/include ; then
+    L="-L/usr/local/lib"
+    trycompile=y
+  fi
+fi
+
+if test $trycompile=y ; then
+  echo "TRY compile "
+  cc $H $testsrc/checkforlibzstd/tzlstd.c $L -lzstd >junkstdout 2>junkstderr
+  if [ $? -eq 0 ]
+  then
+    echo "FOUND: Found libzstd in /usr/local"
+    exit 1
+  fi
 fi
 echo "NOT FOUND: libzstd"
-exit 1
+exit 3
