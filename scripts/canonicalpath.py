357b7e1d42b993c21c1769da97d4e605cf5a31d8
David Anderson <davea42@linuxmail.org>
2023-11-02 10:13:54 -0700

    This now does all name transformations (whether of a single name or a file with pathnames) for independence from locations and systems. 	modified:   scripts/canonicalpath.py

--- scripts/canonicalpath.py
+++ scripts/canonicalpath.py
@@ -3,22 +3,52 @@
 # This code is hereby placed into the public domain.
 
 # try:
-#  canonicalname.py /a/b/../c/d/./e/
+#  canonicalname.py /a/b/../c/d/./e/ $HOME
+# For msys2 mingw this trims off Windows C: stuff
+# msys2 cannot always accept the true Windows path
+# in open().
+# For all else turns the path to the code directory into ...std...
 
 import os
 import sys
 
+def dowinb(s):
+    winb="C:/msys64/"
+    w= s.replace(winb,"/")
+    return w
+
+def printfixedcontent(sname,codepath):
+   for line in open(sname): 
+       w=dowinb(line.rstrip())
+       if not codepath == 'xxxx':
+           s = w.replace(codepath,"...std...")
+           w = s
+       print(w)
+   return
+
 if __name__ == '__main__':
-  #print(len(sys.argv))
-  if len(sys.argv) > 1:
-     s = sys.argv[1]
-     w = os.path.abspath(s)
-     print(w)
-  else:
-     p1="/a/b/c/d/../.././e/"
-     print(p1,os.path.abspath(p1))
-     p2="/a/b/../c//d/./e/"
-     print(p2,os.path.abspath(p2))
-     p3="/a/b/../../d/./e/"
-     print(p3,os.path.abspath(p3))
-     print("no-name-given")
+    #print(len(sys.argv))
+    if len(sys.argv) > 3:
+       s = sys.argv[1]
+       codepath = sys.argv[2]
+       patchwhere = sys.argv[3]
+    else:
+       print("BAD arg count",len(sys.argv),"to canonicalpath.py")
+       sys.exit(1)
+    if patchwhere == "content":
+        printfixedcontent(s,codepath)
+        sys.exit(0)
+    # We are fixing the content 
+    #print("dadebug path",s)
+    w = os.path.abspath(s)
+    #print("dadebug abspath",w)
+    w= dowinb(w)
+    #print("dadebug after dowinb",w)
+    # patchwhere is "start"
+    if not codepath == 'xxxx':
+        #print("dadebug not xxxx ",w)
+        if w.startswith(codepath):
+            s = w.replace(codepath,"...std...")
+            w = s
+        #print("dadebug not xxxx ",w)
+    print(w)
