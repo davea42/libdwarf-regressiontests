114ba629279b80f4d1367f99c2386654753ec827
David Anderson <davea42@linuxmail.org>
2023-11-03 10:41:26 -0700

    Removing print() statements used for debugging. 	modified:   scripts/canonicalpath.py

--- scripts/canonicalpath.py
+++ scripts/canonicalpath.py
@@ -2,12 +2,13 @@
 # Copyright 2020 David Anderson.
 # This code is hereby placed into the public domain.
 
-# try:
-#  canonicalname.py /a/b/../c/d/./e/ $HOME
 # For msys2 mingw this trims off Windows C: stuff
 # msys2 cannot always accept the true Windows path
 # in open().
 # For all else turns the path to the code directory into ...std...
+
+#  test/canonicalpath.py in libdwarf test directory
+#  scripts/canonicalpath.py in regressiontests directory
 
 import os
 import sys
@@ -39,16 +40,11 @@ if __name__ == '__main__':
         printfixedcontent(s,codepath)
         sys.exit(0)
     # We are fixing the content 
-    #print("dadebug path",s)
     w = os.path.abspath(s)
-    #print("dadebug abspath",w)
     w= dowinb(w)
-    #print("dadebug after dowinb",w)
     # patchwhere is "start"
     if not codepath == 'xxxx':
-        #print("dadebug not xxxx ",w)
         if w.startswith(codepath):
             s = w.replace(codepath,"...std...")
             w = s
-        #print("dadebug not xxxx ",w)
     print(w)
