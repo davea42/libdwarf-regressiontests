8d65a3f6ea7fd2e28515467dfae1b4ed5161cf69
David Anderson <davea42@linuxmail.org>
2025-09-01 09:13:38 -0700

    Removed irrelevant #include and #define. 	modified:   test_pubsreader.c

--- test_pubsreader.c
+++ test_pubsreader.c
@@ -19,42 +19,14 @@
 #if defined(_WIN32) && defined(HAVE_STDAFX_H)
 #include "stdafx.h"
 #endif /* HAVE_STDAFX_H */
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h> /* For open() */
-#endif /* HAVE_SYS_TYPES_H */
-#ifdef HAVE_SYS_STAT_H
-#include <sys/stat.h>  /* For open() */
-#endif /* HAVE_SYS_STAT_H */
 #include <fcntl.h>     /* For open() */
 #include <stdlib.h> /* for exit() */
-#ifdef _WIN32
-#include <io.h> /* for close() */
-#elif defined HAVE_UNISTD_H
-#include <unistd.h> /* for close() */
-#endif /* _WIN32 */
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#ifdef HAVE_STDINT_H
-#include <stdint.h> /* For uintptr_t */
-#endif /* HAVE_STDINT_H */
 #include "dwarf.h"
 #include "libdwarf.h"
 #include "libdwarf_private.h"
-
-#ifndef O_RDONLY
-/*  This is for a Windows environment */
-# define O_RDONLY _O_RDONLY
-#endif
-
-#ifdef _O_BINARY
-/*  This is for a Windows environment */
-#define O_BINARY _O_BINARY
-#else
-# ifndef O_BINARY
-# define O_BINARY 0  /* So it does nothing in Linux/Unix */
-# endif
-#endif /* O_BINARY */
 
 #define TRUE 1
 #define FALSE 0
