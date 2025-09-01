d218df3bdaf72e9d9cf252dfb9f32a4e809a91a0
David Anderson <davea42@linuxmail.org>
2025-09-01 09:55:41 -0700

    Heavily revised and simplified for its current use. 	modified:   scripts/dirtostd.py

--- scripts/dirtostd.py
+++ scripts/dirtostd.py
@@ -2,49 +2,56 @@
 # Copyright 2020 David Anderson.
 # This code is hereby placed into the public domain.
 
-# For msys2 mingw this trims off Windows C: stuff
-# msys2 cannot always accept the true Windows path
-# in open().
-# For all else turns the path to the code directory into ...std...
+#  Alters each full-path string in the named input file
+#  so that the prefix such as 
 
 #  test/canonicalpath.py in libdwarf test directory
-#  scripts/canonicalpath.py in regressiontests directory
+#  scripts/dirtostd.py in regressiontests directory
 
 import os
 import sys
 
-def dowinb(s):
-    winb="C:/msys64/davea/"
-    w= s.replace(winb,"/")
-    return w
+#An example path:
+#/home/davea/dwarf/regressiontests/../code/test/.debug/dummyexecutable.debug
+#desired value: /home/davea/dwarf/code/test/.debug/dummyexecutable.debug
+#/home/davea/dwarf/regressiontests/../code/test/.debug/dummyexecutable.debug
+
+#First change "/regressiontests/../code/" to /code/
+
+# example codepath: /home/davea/..../something
+# example codepath: /home/linux1/
+
+wina="C:/msys64/davea/home/admin/dwarf/code"
+winb="C:/msys64/davea/"
+isnormal="...std..."
+
+precanonic ="/regressiontests/../code/"
+postcanonic="/code/"
+
+def dowinb(s,codepath):
+    # for Msys2,desired result
+    w = s.replace(wina,isnormal)
+    # for Msys2, ok result fixed by direct caller.
+    w2 = w.replace(winb,"/")
+    w4 = w2.replace(codepath,isnormal)
+    return w4
 
 def printfixedcontent(sname,codepath):
    for line in open(sname): 
-       w=dowinb(line.rstrip())
-       if not codepath == 'xxxx':
-           s = w.replace(codepath,"...std...")
-           w = s
+       s = line.rstrip()
+       w2 = s.replace(precanonic,postcanonic)
+       #print("dadebug Post canonic ",w2)
+       w=dowinb(w2,codepath)
+       #print("Final line canonic ",w)
+       #print("dadebug w post canonic",w)
        print(w)
    return
 
 if __name__ == '__main__':
-    #print(len(sys.argv))
-    if len(sys.argv) > 3:
-       s = sys.argv[1]
-       codepath = sys.argv[2]
-       patchwhere = sys.argv[3]
+    if len(sys.argv) > 2:
+       s = sys.argv[1] # name of file with text content
+       codepath = sys.argv[2] # the path prefix to change to ...std...
     else:
-       print("BAD arg count",len(sys.argv),"to canonicalpath.py")
+       print("BAD arg count",len(sys.argv),"to dirtostd.py")
        sys.exit(1)
-    if patchwhere == "content":
-        printfixedcontent(s,codepath)
-        sys.exit(0)
-    # We are fixing the content 
-    w = os.path.abspath(s)
-    w= dowinb(w)
-    # patchwhere is "start"
-    if not codepath == 'xxxx':
-        if w.startswith(codepath):
-            s = w.replace(codepath,"...std...")
-            w = s
-    print(w)
+    printfixedcontent(s,codepath)
